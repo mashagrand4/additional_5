d8f50870b92264ea433498b14928478fb56f21f3
Мария <mashagrand4@gmail.com>
2017-09-27 23:58:24 +0300

    brackets/feature: implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ module.exports = function check(str, bracketsConfig) {
+     var stack = [];
+     var myStr = str.split('');
+
+     if (myStr.length % 2 != 0) {
+         return false;
+     }
+
+     for (var i = 0; i < myStr.length; i++) {
+         for (var j = 0; j < bracketsConfig.length; j++) {
+             if (stack.length != 0 && stack[stack.length - 1] == bracketsConfig[j][0] && myStr[i] == bracketsConfig[j][1]) {
+                 stack.pop();
+             }else if (myStr[i] == bracketsConfig[j][0]) {
+                 stack.push(myStr[i]);
+             }
+         }
+     }
+     if (stack.length == 0) {
+        return true;
+     }else{
+         return false;
+     }
+ }
